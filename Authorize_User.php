9670540798ac2eff677895d6c76275b4a57e531d
vakulkin <anton.vakulkin@gmail.com>
2024-10-02 15:01:41 +0200

    first commit

---
+++ Authorize_User.php
@@ -0,0 +1,37 @@
+<?php
+/*
+Plugin Name: Authorize User Local
+Description: Authorizes the user with ID 1 (for local only)
+Author: TheNewLook
+*/
+
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class Authorize_User
+{
+    public function __construct()
+    {
+        add_action('rest_api_init', [$this, 'authorize_user'], 1);
+    }
+
+    public function authorize_user($request)
+    {
+        // var_dump($request);
+        
+        // $x_wp_nonce = $request->get_header("X-WP-Nonce");
+        // echo $x_wp_nonce;
+
+        if (!is_user_logged_in()) {
+            $user_id = 1;
+            $user = get_user_by('id', $user_id);
+            if ($user) {
+                wp_set_current_user($user_id);
+            }
+        }
+    }
+}
+
+new Authorize_User();
